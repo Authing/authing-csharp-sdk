fc684fe8699d9adf529b4243eab7c8b076c02fd9
祁冬 <qidong@authing.cn>
2022-08-16 17:18:01 +0800

    fix: http 服务：添加为空的判断 feat: 认证侧：添加获取用户信息的接口

---
+++ Authing.CSharp.SDK/Authing.CSharp.SDK/Services/AuthenticationClient.Profile.cs
@@ -0,0 +1,25 @@
+﻿using Authing.CSharp.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authing.CSharp.SDK.Services
+{
+    public partial class AuthenticationClient
+    {
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <returns></returns>
+        public async Task<UserSingleRespDto> GetProfile()
+        {
+            string json = await GetAsync("/api/v3/get-profile", string.Empty,AccessToken);
+
+            UserSingleRespDto result = m_JsonService.DeserializeObject<UserSingleRespDto>(json);
+
+            return result;
+        }
+    }
+}
