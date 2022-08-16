e0780e2d673f872253e92608b431e28a7b341e03
祁冬 <qidong@authing.cn>
2022-08-16 16:28:53 +0800

    feat: 认证侧：注册接口接入

--- Authing.CSharp.SDK/Authing.CSharp.SDK/Models/Authentication/LoginTokenRespDto.cs
+++ Authing.CSharp.SDK/Authing.CSharp.SDK/Models/Authentication/LoginTokenRespDto.cs
@@ -33,52 +33,6 @@ namespace Authing.CSharp.SDK.Models
 
     public class LoginTokenResp
     {
-        //[JsonProperty("userId")]
-        //public string UserId { get; set; }
-
-        //[JsonProperty("createdAt")]
-        //public string CreatedAt { get; set; }
-
-        //[JsonProperty("updatedAt")]
-        //public string updatedAt { get; set; }
-
-        //[JsonProperty("status")]
-        //public Status Status { get; set; }
-
-        //public string Email { get; set; }
-
-        //public string Phone { get; set; }
-        //public string PhoneCountryCode { get; set; }
-
-        //public string UserName { get; set; }
-        //public string Name { get; set; }
-        //public string NickName { get; set; }
-        //public string Photo { get; set;  }
-        //public int LoginCount { get; set; }
-        //public string LastLogin { get; set; }
-        //public string LastIp { get;set; }
-        //public Gender Gender { get; set; }
-        //public bool EmailVerified { get; set; }
-        //public bool PhoneVerified { get; set; }
-        //public string PasswordLastSetAt { get; set; }
-        //public string BirthDate { get; set; }
-        //public string Country { get; set; }
-        //public string Province { get; set; }
-        //public string City { get; set; }
-        //public string Address { get; set; }
-        //public string StreetAddress { get; set; }
-        //public string PostalCode { get; set; }
-        //public string ExternalId { get; set; }
-
-        //public bool ResetPasswordOnNextLogin { get; set; }
-        //public List<string > DepartmentIds { get; set; }
-
-        //public List<IdentityDto> Identities { get; set; } 
-
-        //public object CustomData { get; set; }
-
-        //public string StatusChangedAt { get; set; }
-
         /// <summary>
         /// 接口调用凭据，在限制时间内被授权访问资源 API
         /// </summary>
---
+++ Authing.CSharp.SDK/Authing.CSharp.SDK/Services/AuthenticationClient.Signup.cs
@@ -0,0 +1,39 @@
+﻿using Authing.CSharp.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authing.CSharp.SDK.Services
+{
+    public partial class AuthenticationClient
+    {
+        /// <summary>
+        /// 注册
+        /// 此端点目前支持以下几种基于的注册方式： 
+        /// 基于密码（PASSWORD）：用户名 + 密码，邮箱 + 密码。
+        /// 基于一次性临时验证码（PASSCODE）：手机号 + 验证码，邮箱 + 验证码。
+        /// 你需要先调用发送短信或者发送邮件接口获取验证码。社会化登录等使用外部身份源“注册”请直接使用登录接口，我们会在其第一次登录的时候为其创建一个新账号。
+        /// </summary>
+        /// <param name="signupDto"></param>
+        /// <returns></returns>
+        public async Task<UserSingleRespDto> Signup(SignupDto signupDto)
+        {
+            if (signupDto.Connection == SignupConnection.PASSWORD && string.IsNullOrWhiteSpace(signupDto.PasswordPayLoad.Password))
+            {
+                throw new ArgumentException("PasswordPayLoad.Password 不能为空");
+            }
+            if (signupDto.Connection == SignupConnection.PASSCODE && string.IsNullOrWhiteSpace(signupDto.PassCodePayLoad.PassCode))
+            {
+                throw new ArgumentException("PassCodePayLoad.PassCode 不能为空");
+            }
+
+            string json = await PostAsync("/api/v3/signup", signupDto);
+
+            UserSingleRespDto result = m_JsonService.DeserializeObject<UserSingleRespDto>(json);
+
+            return result;
+        }
+    }
+}
