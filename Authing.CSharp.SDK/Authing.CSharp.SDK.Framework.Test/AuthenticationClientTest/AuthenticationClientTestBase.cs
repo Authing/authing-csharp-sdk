148e539ebab9f449773098c149b59f3ff88e6103
TMGG <tm574378328@gmail.com>
2022-08-19 14:24:35 +0800

    fix: Remove something

--- Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientTestBase.cs
+++ Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientTestBase.cs
@@ -24,11 +24,11 @@ namespace Authing.CSharp.SDK.Framework.Test.AuthenticationClientTest
                 //RediretUri = "https://console.mysql.authing-inc.co/console/get-started/62fca2e367113211c9a310a7",
 
                 #region TMGG
-                AppId = "62fde0d10a9d1d7388468e46",
-                AppSecret = "5f6c3b9b47973bf872ba6762496b9b32",
-                Domain = @"https://oijakpbocgfn-demo.test.authing-inc.co",
-                Host = @"https://console.test.authing-inc.co",
-                RediretUri = "https://console.test.authing-inc.co/console/get-started/62fde0d10a9d1d7388468e46",
+                AppId = "AUTHING_APP_ID",
+                AppSecret = "AUTHING_SECRET",
+                Domain = @"AUTHING_DOMAIN",
+                Host = @"AUTHING_HOST",
+                RediretUri = "AUTHING_REDIRECTURI",
                 #endregion
             });
         }
