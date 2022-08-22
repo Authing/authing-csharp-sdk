f6d2707f200ec43dd9dce524ad1fe1a7be4169ab
qidong <qidong@authing.cn>
2022-08-22 11:34:01 +0800

    fix: mamagementclient test
    add test file

---
+++ Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/Management/ManagementClientBaseTest.cs
@@ -0,0 +1,37 @@
+﻿using Authing.CSharp.SDK.Models;
+using Authing.CSharp.SDK.Services;
+using Authing.CSharp.SDK.Utils;
+using Authing.CSharp.SDK.UtilsImpl;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authing.CSharp.SDK.Framework.Test
+{
+    public class ManagementClientBaseTest
+    {
+       protected ManagementClient managementClient;
+
+       protected IDateTimeService dateTimeService;
+
+        protected string UserOneId = "629487c14604f5ca85cbff80";
+        protected string UserTwoId = "61c17bd024917805ae85e397";
+
+        [SetUp]
+        public void Setup()
+        {
+            ManagementClientOptions options = new ManagementClientOptions()
+            {
+                AccessKeyId = "613189b2eed393affbbf396e",
+                AccessKeySecret = "AUTHING_SECRET"
+            };
+
+            managementClient = new ManagementClient(options);
+
+            dateTimeService = new DateTimeService();
+        }
+    }
+}
