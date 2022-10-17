d8110b9470e213738df4815060d1dddf21248b3c
TangMing <tangming@authing.cn>
2022-10-17 16:53:42 +0800

    test: 认证端测试: 添加邮箱验证码注册测试用例

--- Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientAuthTest.Register.cs
+++ Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientAuthTest.Register.cs
@@ -10,11 +10,34 @@ namespace Authing.CSharp.SDK.Framework.Test
 {
     public partial class AuthenticationClientAuthTest
     {
+        /// <summary>
+        /// 2022-10-17 测试通过
+        /// 使用邮箱密码注册
+        /// </summary>
+        /// <returns></returns>
         [Test]
         public async Task RegisterByEmailAndPassword()
         {
             var res = await client.SignUpByEmailPassword("574378328@qq.com", "88886666");
-            Assert.AreEqual(res.StatusCode, 200);
+            Assert.AreEqual(200, res.StatusCode);
+        }
+
+        /// <summary>
+        /// 2022-10-17 测试失败
+        /// 使用邮箱验证码注册
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task RegisterByEmailAndCode()
+        {
+            var res1 = await client.SendEmail(new SendEmailDto()
+            {
+                Email = "574378328@qq.com",
+                Channel = SendEmailDto.channel.CHANNEL_REGISTER
+            });
+            Assert.AreEqual(200,res1.StatusCode);
+            var res2 = await client.SignUpByEmailCode("574378328@qq.com", "6002");
+            Assert.AreEqual(200,res2.StatusCode);
         }
     }
 }
