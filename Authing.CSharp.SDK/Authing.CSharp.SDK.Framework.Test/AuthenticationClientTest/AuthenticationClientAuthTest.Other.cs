9f867fe7a8beb6fe7ff55cd4603390edb7fd8435
TangMing <tangming@authing.cn>
2022-10-19 11:37:23 +0800

    test: 认证端测试: 添加用户其他操作测试用例

--- Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientAuthTest.Other.cs
+++ Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientAuthTest.Other.cs
@@ -170,5 +170,207 @@ namespace Authing.CSharp.SDK.Framework.Test.AuthenticationClientTest
             Assert.AreEqual(200, res.StatusCode);
         }
 
+        /// <summary>
+        /// 2022-10-19 测试成功
+        /// 获取密码强度和账号安全等级评分
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetSecurityLevelTest()
+        {
+            var res = await client.GetSecurityLevel();
+            Assert.AreEqual(200, res.StatusCode);
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试成功
+        /// 修改用户密码
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetUpdatePasswordTest()
+        {
+            var res = await client.UpdatePassword(new UpdatePasswordDto()
+            {
+                NewPassword = "19950630@tm",
+                OldPassword = "88886666",
+                PasswordEncryptType = UpdatePasswordDto.passwordEncryptType.NONE
+            });
+            Assert.AreEqual(200, res.StatusCode);
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试成功
+        /// 修改用户绑定邮箱
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task VerifyUpdateEmailRequestTest()
+        {
+            var res0 = await client.SendEmail(new SendEmailDto()
+            {
+                Channel = SendEmailDto.channel.CHANNEL_UPDATE_EMAIL,
+                Email = "tm574378328@gmail.com"
+            });
+            Assert.AreEqual(200, res0.StatusCode);
+            var res1 = await client.SendEmail(new SendEmailDto()
+            {
+                Channel = SendEmailDto.channel.CHANNEL_UPDATE_EMAIL,
+                Email = "574378328@qq.com"
+            });
+            Assert.AreEqual(200, res1.StatusCode);
+            var res2 = await client.VerifyUpdateEmailRequest(new VerifyUpdateEmailRequestDto()
+            {
+                EmailPasscodePayload = new UpdateEmailByEmailPassCodeDto()
+                {
+                    NewEmail = "tm574378328@gmail.com",
+                    NewEmailPassCode = "5908",
+                    OldEmail = "574378328@qq.com",
+                    OldEmailPassCode = "2925"
+                },
+                VerifyMethod = VerifyUpdateEmailRequestDto.verifyMethod.EMAIL_PASSCODE
+            });
+            Assert.AreEqual(200, res2.StatusCode);
+            var res3 = await client.UpdateEmail(new UpdateEmailDto()
+            {
+                UpdateEmailToken = res2.Data.UpdateEmailToken
+            });
+            Assert.AreEqual(200, res3.StatusCode);
+        }
+
+        /// <summary>
+        /// TODO:修改用户绑定手机号
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task VerifyUpdatePhoneRequestTest()
+        {
+            var res0 = await client.SendSms(new SendSMSDto()
+            {
+                Channel = SendSMSDto.channel.CHANNEL_UNBIND_PHONE,
+                PhoneNumber = "17665662048"
+            });
+            Assert.AreEqual(200, res0.StatusCode);
+            var res1 = await client.SendSms(new SendSMSDto()
+            {
+                Channel = SendSMSDto.channel.CHANNEL_UNBIND_PHONE,
+                PhoneNumber = "17665662048"
+            });
+            Assert.AreEqual(200, res1.StatusCode);
+            var res2 = await client.VerifyUpdatePhoneRequest(new VerifyUpdatePhoneRequestDto()
+            {
+                PhonePassCodePayload = new UpdatePhoneByPhonePassCodeDto()
+                {
+                    NewPhoneNumber = "17665662048",
+                    NewPhonePassCode = "5908",
+                    OldPhoneNumber = "17665662048",
+                    OldPhonePassCode = "2925"
+                },
+                VerifyMethod = VerifyUpdatePhoneRequestDto.verifyMethod.PHONE_PASSCODE
+            });
+            Assert.AreEqual(200, res2.StatusCode);
+            var res3 = await client.UpdatePhone(new UpdatePhoneDto()
+            {
+                UpdatePhoneToken = res2.Data.UpdatePhoneToken
+            });
+            Assert.AreEqual(200, res3.StatusCode);
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试成功
+        /// 忘记密码
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task VerifyResetPasswordRequestTest()
+        {
+            var res = await client.SendEmail(new SendEmailDto()
+            {
+                Channel = SendEmailDto.channel.CHANNEL_RESET_PASSWORD,
+                Email = "tm574378328@gmail.com"
+            });
+            Assert.AreEqual(200, res.StatusCode);
+            var res2 = await client.VerifyResetPasswordRequest(new VerifyResetPasswordRequestDto
+            {
+                VerifyMethod = VerifyResetPasswordRequestDto.verifyMethod.EMAIL_PASSCODE,
+                EmailPassCodePayload = new ResetPasswordByEmailPassCodeDto()
+                {
+                    Email = "tm574378328@gmail.com",
+                    PassCode = "5167"
+                }
+            });
+            Assert.AreEqual(200, res2.StatusCode);
+            var res3 = await client.ResetPassword(new ResetPasswordDto
+            {
+                Password = "88886666",
+                PasswordResetToken = res2.Data.PasswordResetToken,
+                PasswordEncryptType = ResetPasswordDto.passwordEncryptType.NONE,
+            });
+            Assert.AreEqual(200, res3.StatusCode);
+        }
+
+        /// <summary>
+        /// TODO:发起注销账户请求
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task VeirfyDeleteAccountRequestTest()
+        {
+        }
+
+        /// <summary>
+        /// TODO:获取应用公开配置
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetApplicationPublicConfig()
+        {
+            var res = await client.GetApplicationPublicConfig();
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试成功
+        /// 获取服务器公开信息
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetSystemInfoTest()
+        {
+            var res = await client.GetSystemInfo();
+            Assert.IsNotNull(res);
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试失败
+        /// 获取国家列表
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetCountryListTest()
+        {
+            var res = await client.GetCountryList();
+            Assert.IsNotNull(res);
+        }
+
+        /// <summary>
+        /// 2022-10-19 测试失败
+        /// 预检验验证码是否正确
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task PreCheckCodeTest()
+        {
+            var res = await client.PreCheckCode(new PreCheckCodeDto
+            {
+                CodeType = PreCheckCodeDto.codeType.EMAIL,
+                EmailCodePayload =
+                {
+                    Channel = PreCheckEmailCodeDto.channel.CHANNEL_VERIFY_EMAIL_LINK,
+                    Email = "574378328@qq.com",
+                    PassCode = ""
+                }
+            });
+            Assert.AreEqual(200, res.StatusCode);
+        }
     }
 }
