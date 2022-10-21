0c40e31421467fd07f93165ab27d6a0c3ef466b0
TangMing <tangming@authing.cn>
2022-10-21 17:52:35 +0800

    test: AuthticationClient Test: Add token test

---
+++ Authing.CSharp.SDK/Authing.CSharp.SDK.Framework.Test/AuthenticationClientTest/AuthenticationClientAuthTest.Token.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authing.CSharp.SDK.Models;
+using NUnit.Framework;
+
+namespace Authing.CSharp.SDK.Framework.Test
+{
+    public partial class AuthenticationClientAuthTest
+    {
+        [Test]
+        public async Task introspectionTokenTest()
+        {
+            var res = await client.IntrospectToken(AccessToken);
+            Assert.IsTrue(res.Active);
+        }
+        [Test]
+        public async Task revocationTest()
+        {
+            var res = await client.RevokeToken(AccessToken);
+        }
+    }
+}
